c8968c9c50a030ab964dbe682c8d6f867207eeb2
Sarah Heisselberg <sarah_heisselberg@hotmail.com>
2016-09-25 18:17:24 +0200

    Adding the Homepage
    Creating the homepage for the website by naming it “index.php”, and
    linking it to an external CSS stylesheet using the link rel= tag in the
    head section of the code. The PHP menu (name “menu.php”) is included in
    the body section using the PHP string <?php include ‘menu.php’; ?>

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="../php-menu/stylesheet.css">
+<title>Home</title>
+</head>
+
+<body>
+<?php include '../php-menu/menu.php'; ?>
+
+<div id='home-title'>
+<p>Do you know how to code PHP?</p>
+</div>
+
+</body>
+</html>
